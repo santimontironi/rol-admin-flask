0ce58afcc5291a1aba4f4ef529e82f8d6590379f
santimontironi <santiimontironi@gmail.com>
2025-02-20 17:46:16 -0300

    funcion eliminar noticia agregada

--- routes/noticias.py
+++ routes/noticias.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request,redirect,url_for
 from werkzeug.utils import secure_filename
 import os
 from models.bd import mysql
@@ -14,12 +14,14 @@ def agregar_noticias():
         imagenNoticia = request.files['imagenNoticia']
 
         nombre_seguro_imagen = secure_filename(imagenNoticia.filename)
-        rutaImagen = os.path.join('static/uploads', nombre_seguro_imagen)
+        rutaImagen = os.path.join('../empleados/static/uploads', nombre_seguro_imagen)
         imagenNoticia.save(rutaImagen)
 
         conexion = mysql.connection.cursor()
         conexion.execute('INSERT INTO noticias(titulo,descripcion,imagen) VALUES(%s,%s,%s)', (tituloNoticia, descripcionNoticia, nombre_seguro_imagen))
         mysql.connection.commit()
+        
+        return redirect(url_for('noticias.agregar_noticias'))
 
     else:
         conexion = mysql.connection.cursor()
@@ -27,3 +29,12 @@ def agregar_noticias():
         noticias = conexion.fetchall()
     
     return render_template('agregar-noticias.html', noticias=noticias)
+
+@noticias_bp.route('/eliminar-noticia',methods=['GET','POST'])
+def eliminar_noticia():
+    if request.method == "POST":
+        idNoticia = request.form['noticiaId']
+        conexion = mysql.connection.cursor()
+        conexion.execute('UPDATE noticias SET estado = "eliminada" WHERE id_noticia = %s',(idNoticia,))
+        mysql.connection.commit()
+        return redirect(url_for('noticias.agregar_noticias'))
